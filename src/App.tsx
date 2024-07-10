da93453d2ee805f5592a03f5923923b6ba7047c8
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-07-09 21:13:28 -0500

    feat(app, db,types, MenuItem): typesMenu
    * se creo el main donde se renderiza en menu todos los items
    * se creo el type del menu y se agrego a la DB
    * MenuItems es cada uno de los Items del menu y como se va a mostrarlo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import MenuItem from "./components/MenuItem"
+import { menuItems } from "./data/db"
 function App() {
   
   return (
@@ -6,6 +7,20 @@ function App() {
         <header className="bg-teal-400 py-5"> 
         <h1 className="text-center text-4xl font-black">Calculadora de propinas</h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="flex justify-center text-center text-4xl font-black">Menu</h2>
+          {menuItems.map((item) => (
+            <MenuItem 
+            key={item.id}
+            item={item}
+            />
+          ))}
+        </div>
+        <div>
+        <h2 className="flex justify-center text-center text-4xl font-black">Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,16 @@
+import { MenuItems } from "../types";
+type MenuItemProps = {
+  item: MenuItems
+}
+
+export default function MenuItem({item}: MenuItemProps) {
+  const {name, price} = item // Destructuring
+  return (
+  <button 
+  className="border-2 border-teal-400 p-4 w-full flex justify-between items-center my-2 rounded-lg hover:bg-teal-400 hover:text-white transition duration-300 ease-in-out"
+  >
+    <p>{name}</p>
+    <p className="font-black">{price}</p>
+  </button>
+  )
+}
