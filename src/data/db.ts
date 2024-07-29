644594d9d09a62526d4ec97d406d8124e8f29d00
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-07-29 18:38:37 -0500

    refactor(DB, types ): agregando imagen a productos, modificando types
    lo que se realizo fue que se añadio una imagen de cada producto en la bd y se modifico el tipo

--- src/data/db.ts
+++ src/data/db.ts
@@ -3,61 +3,73 @@ import type { MenuItems } from "../types";
 export const menuItems: MenuItems[] = [
   {
     id: 1,
+    image: "🍕",
     name: "Pizza a la Leña Chica",
     price: 30,
   },
   {
     id: 2,
+    image: "🍕",
     name: "Pizza a la Leña Mediana",
     price: 50,
   },
   {
     id: 3,
+    image: "🥧",
     name: "Rebanada de Pay de Limón",
     price: 30,
   },
   {
     id: 4,
+    image: "🥮",
     name: "Rebanada de Pastel de Chocolate",
     price: 30,
   },
   {
     id: 5,
+    image: "🍹",
     name: "Jugo de Naranja",
     price: 15,
   },
   {
     id: 6,
+    image: "🍕",
     name: "Pizza a la Leña Grande",
     price: 70,
   },
   {
     id: 7,
+    image: "🥩",
     name: "Rib Eye 800g",
     price: 100,
   },
   {
     id: 8,
+    image: "🍹",
     name: "Jugo de Naranja",
     price: 15,
   },
   {
     id: 9,
+    image: "🍸",
     name: "Tequila",
     price: 40,
   },
   {
     id: 10,
+    image: "🥧",
     name: "Rebanada de Pay de Queso",
     price: 30,
   },
   {
     id: 11,
+    image: "☕️",
     name: "Café Americano",
     price: 20,
   },
   {
     id: 12,
+    image: "☕️",
     name: "Café Capuchino",
     price: 40,
   },
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export type MenuItems = {
   id: number;
+  image: string;
   name: string;
   price: number;
 };
